09a36c68162c46a4530c6e0bdee303b0a41f685b
AmineYoussef <amine.youssef@xevlabs.com>
2021-10-19 15:32:58 +0100

    current user method change
    when test on login it wasn't working. this makes it retrieve the user stored in the session and updates the auth guard accordingly

--- libs/ng-strapi-auth/src/lib/core/services/user/user.service.ts
+++ libs/ng-strapi-auth/src/lib/core/services/user/user.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
-import { Observable } from 'rxjs';
 import { UserModel, DefaultUserModel } from '../../models';
 import { LocalStorageKeyEnum } from '../../enums';
 
@@ -30,10 +28,8 @@ export class UserService {
         sessionStorage.removeItem(LocalStorageKeyEnum.CURRENT_USER);
     }
 
-    getCurrentUser<T = DefaultUserModel>(): Observable<UserModel<T | DefaultUserModel> | null> {
-        return this.authService.authUserChanged$.pipe(map((user) => {
-            return user
-        }))
+    getCurrentUser<T = DefaultUserModel>() {
+        return JSON.parse(sessionStorage.getItem(LocalStorageKeyEnum.CURRENT_USER)!) as UserModel<T>
     }
 
 }
--- libs/ng-strapi-auth/src/lib/routing/auth.guard.ts
+++ libs/ng-strapi-auth/src/lib/routing/auth.guard.ts
@@ -1,12 +1,13 @@
 import { Inject, Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
-import { switchMap, take } from 'rxjs/operators';
-import { UserModel, RoleModel } from '../core/models';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { RoleModel } from '../core/models';
 import { UserService } from '../core/services/user/user.service';
 import { SnackBarService } from '@ng-xevlabs-utils-snackbar';
 import { SnackBarTypeEnum } from 'libs/xevlabs-ng-utils/libs/xevlabs-snackbar/src/lib/enums';
 import { AuthOptionModel } from '../ng-strapi-auth-options';
+import { AuthService } from '@ng-strapi-auth/ng-strapi-auth';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -14,6 +15,7 @@ export class AuthGuard implements CanActivate {
     constructor(
         private router: Router,
         public userService: UserService,
+        public authService: AuthService,
         @Inject('StrapiAuthLibOptions') private readonly options: AuthOptionModel,
         private readonly snackBarService: SnackBarService,
     ) { }
@@ -21,46 +23,37 @@ export class AuthGuard implements CanActivate {
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
         let redirectionRoute: string[] = ['authentication', 'login'];
         let roles = route.data.roles as Array<RoleModel>
-        return this.userService.getCurrentUser().pipe(
-            take(1),
-            switchMap((user: UserModel | null) => {
+        let user = this.userService.getCurrentUser()
                 if (!roles.length) {
-                    return this.router.navigate(redirectionRoute).then(() => {
+                    return of(this.router.navigate(redirectionRoute)).pipe(map(() => {
                         this.snackBarService.showSnackBar(SnackBarTypeEnum.ERROR, 'AUTH.GUARD.NO_ROLES');
                         return false;
-                    });
+                    }));
                 }
                 if (!user) {
-                    return this.router.navigate(redirectionRoute).then(() => {
+                    return of(this.router.navigate(redirectionRoute)).pipe(map(() => {
                         this.snackBarService.showSnackBar(SnackBarTypeEnum.ERROR, 'AUTH.GUARD.LOGIN_REQUIRED');
                         return false;
-                    });
+                    }));
                 }
-                if (!roles!.includes(user!.role)) {
-                    return this.router.navigate(redirectionRoute).then(() => {
+                if (!roles!.filter(role => role.id === user!.role.id).length) {
                         this.snackBarService.showSnackBar(SnackBarTypeEnum.ERROR, 'AUTH.GUARD.WRONG_ROLE');
-                        return false;
-                    });
+                        return of(false);
                 }
 
                 if (user!.blocked) {
-                    return this.router.navigate(redirectionRoute).then(() => {
+                    return of(this.router.navigate(redirectionRoute)).pipe(map(() => {
                         this.snackBarService.showSnackBar(SnackBarTypeEnum.ERROR, 'AUTH.GUARD.USER_BLOCKED');
                         return false;
-                    });
+                    }));
                 }
 
                 if (!user!.confirmed && this.options.blockIfNotConfirmed) {
-                    return this.router.navigate(redirectionRoute).then(() => {
+                    return of(this.router.navigate(redirectionRoute)).pipe(map(() => {
                         this.snackBarService.showSnackBar(SnackBarTypeEnum.ERROR, 'AUTH.GUARD.USER_NOT_CONFIRMED');
                         return false;
-                    });
+                    }));
                 }
-                return this.router.navigate(['']).then(() => {
-                    this.snackBarService.showSnackBar(SnackBarTypeEnum.ERROR, 'AUTH.GUARD.NOT_AUTHORIZED');
-                    return false;
-                });
-            }));
-    }
-
+                return of(true)
+            }
 }
