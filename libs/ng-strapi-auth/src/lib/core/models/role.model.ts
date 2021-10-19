9adfe5ae321b9146ef187f59d80ce8335d4d955a
AmineYoussef <amine.youssef@xevlabs.com>
2021-10-19 11:13:05 +0100

    model typo and missing guard index export

--- libs/ng-strapi-auth/src/lib/core/models/role.model.ts
+++ libs/ng-strapi-auth/src/lib/core/models/role.model.ts
@@ -1,7 +1,7 @@
 import { RoleTypeEnum } from "../enums/role-type.enum";
 
 export interface RoleModel {
-    id: string;
+    id: number;
     name: string;
     description: string;
     type: RoleTypeEnum;
