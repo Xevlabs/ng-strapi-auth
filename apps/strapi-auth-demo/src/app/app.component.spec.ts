9049d8b967288fdfd80d4d37d9bdd1c485ccbc00
Antoine Hervet <antoine@MacBook-Air-de-Antoine.local>
2021-10-29 13:07:30 +0200

    Fixed styles

--- apps/strapi-auth-demo/src/app/app.component.spec.ts
+++ apps/strapi-auth-demo/src/app/app.component.spec.ts
@@ -8,7 +8,7 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('should create the routing', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
