462c27f7cc08df7da58898936d45b4848f92c151
Amine Youssef <amine.youssef@esprit.tn>
2021-11-01 13:51:02 +0100

    added http interceptor export

--- libs/ng-strapi-auth/src/index.ts
+++ libs/ng-strapi-auth/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/core/models/index';
 export * from './lib/core/enums/index';
 export * from './lib/routing/auth.guard';
 export * from './lib/core/services'
+export * from './lib/core/interceptor/http.auth-interceptor';
