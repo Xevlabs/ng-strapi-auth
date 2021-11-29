d7387e178b6618766bd8312ffd3a29cd9bebec01
AmineYoussef <amine.youssef@xevlabs.com>
2021-11-29 09:57:42 +0100

    added non-auth routing

--- libs/ng-strapi-auth/src/index.ts
+++ libs/ng-strapi-auth/src/index.ts
@@ -2,6 +2,7 @@ export * from './lib/ng-strapi-auth.module';
 export * from './lib/core/models/index';
 export * from './lib/core/enums/index';
 export * from './lib/routing/auth.guard';
+export * from './lib/routing/non-auth.guard';
 export * from './lib/core/services'
 export * from './lib/core/interceptor/http.auth-interceptor';
 export * from './lib/core/custom-validators/confirm-password-validator.directive';
