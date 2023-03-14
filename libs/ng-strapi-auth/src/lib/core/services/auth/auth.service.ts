0d2c2e189204ff68cb768eeb33ef92c23fd2d803
Antoine <antoine.hervet@xevlabs.com>
2023-03-14 17:42:46 +0100

    Fixed indents

--- libs/ng-strapi-auth/src/lib/core/services/auth/auth.service.ts
+++ libs/ng-strapi-auth/src/lib/core/services/auth/auth.service.ts
@@ -10,108 +10,108 @@ import {AuthResponseModel} from '../../models/auth-response.model';
 import {TranslocoService} from '@ngneat/transloco';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
-  authToken: string | null;
-  allowedRoles: string[]
-  authApiBase: string;
-  authUserChanged$: Subject<UserModel | null> = new ReplaySubject<UserModel | null>(0);
+    authToken: string | null;
+    allowedRoles: string[]
+    authApiBase: string;
+    authUserChanged$: Subject<UserModel | null> = new ReplaySubject<UserModel | null>(0);
 
-  constructor(
+    constructor(
     private httpClient: HttpClient,
     @Inject('StrapiAuthLibOptions') private readonly options: AuthOptionModel,
     private readonly translocoService: TranslocoService,
     public router: Router,
-  ) {
-    this.allowedRoles = this.options.roleList;
-    this.authApiBase = this.options.baseAPIPath;
-    this.authToken = localStorage.getItem(LocalStorageKeyEnum.CURRENT_JWT);
-    if (this.authToken) {
-      this.getUserFromServer().pipe(take(1)).subscribe((user) => {
-        this.authUserChanged$.next(this.authToken ? user : null)
-      })
-    } else {
-      this.authUserChanged$.next(null)
+    ) {
+        this.allowedRoles = this.options.roleList;
+        this.authApiBase = this.options.baseAPIPath;
+        this.authToken = localStorage.getItem(LocalStorageKeyEnum.CURRENT_JWT);
+        if (this.authToken) {
+            this.getUserFromServer().pipe(take(1)).subscribe((user) => {
+                this.authUserChanged$.next(this.authToken ? user : null)
+            })
+        } else {
+            this.authUserChanged$.next(null)
+        }
     }
-  }
 
-  login<T = DefaultUserModel>(username: string, password: string): Observable<UserModel<T>> {
-    let jwt: string;
-    return this.httpClient.post<any>(`${this.authApiBase}/auth/local`, {identifier: username, password: password})
-      .pipe(
-        switchMap((data: AuthResponseModel<T>) => {
-          if (data.jwt) {
-            jwt = data.jwt;
-            return this.getUserFromServer<T>(jwt);
-          }
-          throw Error(this.translocoService.translate("TOKEN.NOT.EXIST"));
-        }),
-        map((user: UserModel<T> | null) => {
-          if (user === null) {
-            this.authUserChanged$.next(null);
-            throw Error(this.translocoService.translate("AUTH.USER.NOT.EXIST"));
-          }
+    login<T = DefaultUserModel>(username: string, password: string): Observable<UserModel<T>> {
+        let jwt: string;
+        return this.httpClient.post<any>(`${this.authApiBase}/auth/local`, {identifier: username, password: password})
+            .pipe(
+                switchMap((data: AuthResponseModel<T>) => {
+                    if (data.jwt) {
+                        jwt = data.jwt;
+                        return this.getUserFromServer<T>(jwt);
+                    }
+                    throw Error(this.translocoService.translate("TOKEN.NOT.EXIST"));
+                }),
+                map((user: UserModel<T> | null) => {
+                    if (user === null) {
+                        this.authUserChanged$.next(null);
+                        throw Error(this.translocoService.translate("AUTH.USER.NOT.EXIST"));
+                    }
 
-          if (user.blocked) {
-            this.authUserChanged$.next(null);
-            throw Error(this.translocoService.translate("AUTH.USER.BLOCKED"));
-          }
+                    if (user.blocked) {
+                        this.authUserChanged$.next(null);
+                        throw Error(this.translocoService.translate("AUTH.USER.BLOCKED"));
+                    }
 
-          if (this.allowedRoles && !this.allowedRoles.includes(user.role.name)) {
-            this.authUserChanged$.next(null);
-            throw Error(this.translocoService.translate("AUTH.USER.NOT.ALLOWED"));
-          }
+                    if (this.allowedRoles && !this.allowedRoles.includes(user.role.name)) {
+                        this.authUserChanged$.next(null);
+                        throw Error(this.translocoService.translate("AUTH.USER.NOT.ALLOWED"));
+                    }
 
-          localStorage.setItem(LocalStorageKeyEnum.CURRENT_JWT, jwt);
-          this.authUserChanged$.next(user);
-          this.authToken = localStorage.getItem(LocalStorageKeyEnum.CURRENT_JWT)!;
-          return user as UserModel<T>;
-        })
-      );
-  }
+                    localStorage.setItem(LocalStorageKeyEnum.CURRENT_JWT, jwt);
+                    this.authUserChanged$.next(user);
+                    this.authToken = localStorage.getItem(LocalStorageKeyEnum.CURRENT_JWT)!;
+                    return user as UserModel<T>;
+                })
+            );
+    }
 
-  loginWithJWT<T = DefaultUserModel>(token: string): Observable<UserModel<T>> {
-    this.authToken = token
-    return this.getUserFromServer<T>().pipe(map((user: UserModel<T>) => {
-      localStorage.setItem(LocalStorageKeyEnum.CURRENT_JWT, token)
-      this.authUserChanged$.next(user);
-      return user
-    }))
-  }
+    loginWithJWT<T = DefaultUserModel>(token: string): Observable<UserModel<T>> {
+        this.authToken = token
+        return this.getUserFromServer<T>().pipe(map((user: UserModel<T>) => {
+            localStorage.setItem(LocalStorageKeyEnum.CURRENT_JWT, token)
+            this.authUserChanged$.next(user);
+            return user
+        }))
+    }
 
-  logout() {
-    localStorage.removeItem(LocalStorageKeyEnum.CURRENT_JWT);
-    this.authUserChanged$.next(null);
-    this.router.navigate(['authentication']);
-  }
+    logout() {
+        localStorage.removeItem(LocalStorageKeyEnum.CURRENT_JWT);
+        this.authUserChanged$.next(null);
+        this.router.navigate(['authentication']);
+    }
 
-  get isLoggedIn(): boolean {
-    const authToken = JSON.parse(localStorage.getItem(LocalStorageKeyEnum.CURRENT_JWT)!);
-    return authToken !== null;
-  }
+    get isLoggedIn(): boolean {
+        const authToken = JSON.parse(localStorage.getItem(LocalStorageKeyEnum.CURRENT_JWT)!);
+        return authToken !== null;
+    }
 
-  registerUser<T = DefaultUserModel, Y = any>(clientInformation: T, apiPath: string) {
-    return this.httpClient.post<Y>(apiPath, clientInformation)
-  }
+    registerUser<T = DefaultUserModel, Y = any>(clientInformation: T, apiPath: string) {
+        return this.httpClient.post<Y>(apiPath, clientInformation)
+    }
 
-  getUserFromServer<T = DefaultUserModel>(jwt?: string) {
-    return this.httpClient.get<UserModel<T>>(`${this.authApiBase}/users/me`,
-      {
-        headers: {
-          Authorization: `Bearer ${jwt ? jwt : this.authToken}`,
-        },
-        params: {populate: 'role'}
-      }).pipe(take(1))
-  }
+    getUserFromServer<T = DefaultUserModel>(jwt?: string) {
+        return this.httpClient.get<UserModel<T>>(`${this.authApiBase}/users/me`,
+            {
+                headers: {
+                    Authorization: `Bearer ${jwt ? jwt : this.authToken}`,
+                },
+                params: {populate: 'role'}
+            }).pipe(take(1))
+    }
 
-  forgotPassword(email: string): Observable<boolean> {
-    return this.httpClient.post<boolean>(`${this.authApiBase}/auth/forgot-password`, {email: email});
-  }
+    forgotPassword(email: string): Observable<boolean> {
+        return this.httpClient.post<boolean>(`${this.authApiBase}/auth/forgot-password`, {email: email});
+    }
 
-  resetPassword(passReset: PassResetModel) {
-    return this.httpClient.post<any>(`${this.authApiBase}/auth/reset-password`, passReset);
-  }
+    resetPassword(passReset: PassResetModel) {
+        return this.httpClient.post<any>(`${this.authApiBase}/auth/reset-password`, passReset);
+    }
 
 }
--- libs/ng-strapi-auth/src/lib/routing/auth.guard.ts
+++ libs/ng-strapi-auth/src/lib/routing/auth.guard.ts
@@ -1,78 +1,78 @@
-import {Inject, Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
-import {Observable, of, share} from 'rxjs';
-import {map} from 'rxjs/operators';
-import {UserService} from '../core/services/user/user.service';
-import {AuthOptionModel} from '../ng-strapi-auth-options';
-import {AuthService} from '../core/services';
-import {HotToastService} from '@ngneat/hot-toast';
-import {TranslocoService} from '@ngneat/transloco';
+import { Inject, Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable, of, share } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { UserService } from '../core/services/user/user.service';
+import { AuthOptionModel } from '../ng-strapi-auth-options';
+import { AuthService } from '../core/services';
+import { HotToastService } from '@ngneat/hot-toast';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(
+    constructor(
     private router: Router,
     public userService: UserService,
     public authService: AuthService,
     @Inject('StrapiAuthLibOptions') private readonly options: AuthOptionModel,
     private hotToastService: HotToastService,
     private translocoService: TranslocoService
-  ) {
-  }
-
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    let redirectionRoute: string[] = ['authentication', 'login'];
-    let roles = route.data.roles as Array<string>
-    let user = this.userService.getCurrentUser()
-
-    if (!roles.length) {
-      this.authService.logout();
-      return of(this.router.navigate(redirectionRoute)).pipe(map(() => {
-        this.hotToastService.error(this.translocoService.translate('AUTH.GUARD.NO_ROLES'));
-        return false;
-      }));
+    ) {
     }
 
-    if (!user) {
-      return this.translocoService.selectTranslate('AUTH.GUARD.LOGIN_REQUIRED').pipe(
-        map((translation: string) => {
-          this.authService.logout();
-          this.hotToastService.error(translation);
-          this.router.navigate(redirectionRoute);
-          return false;
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+        const redirectionRoute: string[] = ['authentication', 'login'];
+        const roles = route.data.roles as Array<string>
+        const user = this.userService.getCurrentUser()
+
+        if (!roles.length) {
+            this.authService.logout();
+            return of(this.router.navigate(redirectionRoute)).pipe(map(() => {
+                this.hotToastService.error(this.translocoService.translate('AUTH.GUARD.NO_ROLES'));
+                return false;
+            }));
         }
-      )
-      )
-    }
 
-    if (!user.role) {
-      this.authService.logout();
-      this.hotToastService.error(this.translocoService.translate('AUTH.GUARD.WRONG_ROLE'));
-      return of(false);
-    }
+        if (!user) {
+            return this.translocoService.selectTranslate('AUTH.GUARD.LOGIN_REQUIRED').pipe(
+                map((translation: string) => {
+                    this.authService.logout();
+                    this.hotToastService.error(translation);
+                    this.router.navigate(redirectionRoute);
+                    return false;
+                }
+                )
+            )
+        }
 
-    if (!roles!.includes(user!.role.name)) {
-      this.authService.logout();
-      this.hotToastService.error(this.translocoService.translate('AUTH.GUARD.WRONG_ROLE'));
-      return of(false);
-    }
+        if (!user.role) {
+            this.authService.logout();
+            this.hotToastService.error(this.translocoService.translate('AUTH.GUARD.WRONG_ROLE'));
+            return of(false);
+        }
 
-    if (user!.blocked) {
-      this.authService.logout();
-      this.hotToastService.error(this.translocoService.translate('AUTH.GUARD.USER_BLOCKED'));
-      return of(this.router.navigate(redirectionRoute)).pipe(map(() => {
-        return false;
-      }));
-    }
+        if (!roles!.includes(user!.role.name)) {
+            this.authService.logout();
+            this.hotToastService.error(this.translocoService.translate('AUTH.GUARD.WRONG_ROLE'));
+            return of(false);
+        }
 
-    if (!user!.confirmed && this.options.blockIfNotConfirmed) {
-      this.authService.logout();
-      this.hotToastService.error(this.translocoService.translate('AUTH.GUARD.USER_NOT_CONFIRMED'));
-      return of(this.router.navigate(redirectionRoute)).pipe(map(() => {
-        return false;
-      }));
+        if (user!.blocked) {
+            this.authService.logout();
+            this.hotToastService.error(this.translocoService.translate('AUTH.GUARD.USER_BLOCKED'));
+            return of(this.router.navigate(redirectionRoute)).pipe(map(() => {
+                return false;
+            }));
+        }
+
+        if (!user!.confirmed && this.options.blockIfNotConfirmed) {
+            this.authService.logout();
+            this.hotToastService.error(this.translocoService.translate('AUTH.GUARD.USER_NOT_CONFIRMED'));
+            return of(this.router.navigate(redirectionRoute)).pipe(map(() => {
+                return false;
+            }));
+        }
+        return of(true)
     }
-    return of(true)
-  }
 }
